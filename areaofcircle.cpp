5287f1e8ab926b3a0531bd3cbbfeb03fa91b459a
ra-hul16 <138222343+ra-hul16@users.noreply.github.com>
2024-04-05 20:14:26 +0530

    Add files via upload

---
+++ areaofcircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void arofcircle(){
+    int r;
+    cout<<"Enter the radius of circle: ";
+    cin>>r;
+    cout<<"Area of Circle: "<<3.14*r*r;
+}
+int main(){
+    arofcircle();
+}
