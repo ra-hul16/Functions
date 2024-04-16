4a5d20bb3b0f4bd9d3de45b836e1e903ef4d085e
ra-hul16 <138222343+ra-hul16@users.noreply.github.com>
2024-04-16 23:46:48 +0530

    Add files via upload

---
+++ LC2469.cpp
@@ -0,0 +1,9 @@
+double K=celsius + 273.15;
+        double F=celsius*1.80+32.00;
+        vector<double> v;
+        v.push_back(K);
+        v.push_back(F);
+        return v;
+
+// Celsius to kelvin and fahrenheit
+//question number : 2469
