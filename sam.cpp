bebab9fc91c559f5d28979e9d0bd5eda8d5e7d97
ra-hul16 <138222343+ra-hul16@users.noreply.github.com>
2024-04-18 20:05:39 +0530

    Add files via upload

---
+++ sam.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    if(n==0||n==1){
+        cout<<INT_MIN;
+    }
+    else{
+        int max = INT_MIN;
+        for(int i=0;i<n;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+        int second = INT_MIN;
+        for(int i=0;i<n;i++){
+            if(max>arr[i] && arr[i]>second){
+                second = arr[i];
+            }
+        }
+        cout<<second;
+    }
+}
+
